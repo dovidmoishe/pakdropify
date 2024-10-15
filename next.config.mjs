6a4a7e402e0e07c4c7c49d927f651983da0620ae
David Baiye <ozerlihashem@gmail.com>
2024-10-15 12:56:25 -0700

    CI/CD

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@ import { hostname } from "os";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
-  assetPrefix: process.env.URL ? process.env.URL : undefined,
   reactStrictMode: true,
   images: {
     remotePatterns: [
