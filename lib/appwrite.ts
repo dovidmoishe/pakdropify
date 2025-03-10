4fc0a6c08caa1cf29911ce4f5f5bb9403fe4a3dd
David Baiye <ozerlihashem@gmail.com>
2025-03-10 16:29:26 +0100

    Update appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Databases, Account } from "appwrite";
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("66be46c000084543c2c2"); // Replace with your project ID
+  .setProject(""); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
